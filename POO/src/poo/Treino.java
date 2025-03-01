118d36246d5aba5a569c3298a21af9cb4c95d922
Diego Amaral <126113489+Ryzuke@users.noreply.github.com>
2025-03-01 18:20:53 -0300

    Add files via upload
    Criação do super "personagem" e da primeira classe que vai extender o personagem, "rastreador".

---
+++ POO/src/poo/Treino.java
@@ -0,0 +1,27 @@
+package poo;
+
+public class Treino {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Aqui eu crio o rastreador Diego que se refere a classe rastreador
+		 */
+		Rastreador diego = new Rastreador();
+		
+		
+		diego.stats();
+		diego.sentirFome();
+		diego.sentirFome();
+		diego.sentirFome();
+		diego.sentirFome();
+		diego.sentirFome();
+		diego.stats();
+		
+		
+		
+
+		
+		
+	}
+}
