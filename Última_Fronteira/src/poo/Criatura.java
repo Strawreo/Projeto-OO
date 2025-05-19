c2610a9b4ce0c6ea9f038747046e256517c84cbf
Eryck Emmanuel <strawreomilk@gmail.com>
2025-05-19 19:23:31 -0300

    Add files via upload

---
+++ Última_Fronteira/src/poo/Criatura.java
@@ -0,0 +1,40 @@
+package poo;
+
+public class Criatura {
+	
+	private String nome;
+	private double vida;
+	private double defesa;
+	
+	public Criatura( String nome,double vida,double defesa) {
+		
+		this.setVida(vida);
+		this.setDefesa(defesa);
+		this.setNome(nome);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getVida() {
+		return vida;
+	}
+
+	public void setVida(double vida) {
+		this.vida = vida;
+	}
+
+	public double getDefesa() {
+		return defesa;
+	}
+
+	public void setDefesa(double defesa) {
+		this.defesa = defesa;
+	}
+
+}
