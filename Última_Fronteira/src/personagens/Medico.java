c97390482bd4ffa369358fbf85698f49700aab06
Eryck Emmanuel <strawreomilk@gmail.com>
2025-05-29 17:17:50 -0300

    Add files via upload

--- Última_Fronteira/src/personagens/Medico.java
+++ Última_Fronteira/src/personagens/Medico.java
@@ -2,7 +2,7 @@ package personagens;
 
 public class Medico extends Personagem{
 	public Medico() {
-		super("Médico","Um médico" + "\n" + "Ao encontrar bandagens elas virão em dobro",10,800,100,100,100,100,20,20,1);
+		super("Médico","Um médico" + "\n" + "Ao encontrar bandagens elas virão em dobro",10,800,100,100,100,100,10,20,1);
 		
 	}
 
