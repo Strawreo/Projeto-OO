5a9de8f6ca9bb52437da779df19e16ca7c8b53fb
Eryck Emmanuel <strawreomilk@gmail.com>
2025-05-31 10:10:33 -0300

    Update Personagem.java

--- Última_Fronteira/src/personagens/Personagem.java
+++ Última_Fronteira/src/personagens/Personagem.java
@@ -57,7 +57,7 @@ public abstract class Personagem {
 		    this.vida -= danoFinal;
 		    if (this.vida < 0) this.vida = 0;
 
-		    System.out.printf(nome + " perdeu %.2f de vida. Vida atual: %.2f \n" danoFinal, vida);
+		    System.out.printf(nome + " perdeu %.2f de vida. Vida atual: %.2f \n" + danoFinal, vida);
 		}
 	 public void sentirFome(int dano) {
 			System.out.println("Sentindo fome...");
