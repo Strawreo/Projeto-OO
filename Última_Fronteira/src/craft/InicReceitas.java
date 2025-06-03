64ddcd427e32d39b7a10e0a5f7f7f11651308fcc
Eryck Emmanuel <strawreomilk@gmail.com>
2025-06-03 12:24:01 -0300

    Implementação do Ferreiro+

--- Última_Fronteira/src/craft/InicReceitas.java
+++ Última_Fronteira/src/craft/InicReceitas.java
@@ -33,11 +33,46 @@ public class InicReceitas {
 			    craftaveis.get(3), //Graveto
 			    craftaveis.get(2)  //Tecido
 			));
-		ArrayList<ClassItem> marretaIng = new ArrayList<>(Arrays.asList( //Mecânico *Exclusivo
+		
+		
+		this.receitas.add(new Receita(espadaDeMadeiraIng,equipaveis.get(15)));
+		this.receitas.add(new Receita(espadaDePedraIng,equipaveis.get(24)));
+		this.receitas.add(new Receita(bandagensIng,usaveis.get(0)));
+		this.receitas.add(new Receita(arcoIng,equipaveis.get(16)));
+		
+		
+		return this.receitas;
+	}
+	
+	public ArrayList<Receita> inicializarReceitasFerr(){
+		InicItensCraft inicCraft = new InicItensCraft();
+		InicItensEquip inicEquip = new InicItensEquip();
+		InicItensUsa inicUsa = new InicItensUsa();
+		
+		ArrayList<ClassItem> craftaveis = inicCraft.inicializarItensCraft();
+		ArrayList<ClassItem> equipaveis = inicEquip.Inicializar_ItensEquip();
+		ArrayList<ClassItem> usaveis = inicUsa.inicializarItensUsa();
+		
+		
+		ArrayList<ClassItem> espadaDeMadeiraIng = new ArrayList<>(Arrays.asList(
+			    craftaveis.get(3) //Graveto
+			));
+		ArrayList<ClassItem> espadaDePedraIng = new ArrayList<>(Arrays.asList(
+			    craftaveis.get(3), //Graveto
+			    craftaveis.get(1) //Pedra
+			));
+		ArrayList<ClassItem> bandagensIng = new ArrayList<>(Arrays.asList(
+			    craftaveis.get(2) //Tecido
+			));
+		ArrayList<ClassItem> arcoIng = new ArrayList<>(Arrays.asList(
+			    craftaveis.get(3), //Graveto
+			    craftaveis.get(2)  //Tecido
+			));
+		ArrayList<ClassItem> marretaIng = new ArrayList<>(Arrays.asList( //Ferreiro *Exclusivo
 			    craftaveis.get(3), //Graveto
 			    craftaveis.get(4)  //Ferro Bruto
 			));
-		ArrayList<ClassItem> espadaDeBronzeIng = new ArrayList<>(Arrays.asList( //Mecânico 
+		ArrayList<ClassItem> espadaDeBronzeIng = new ArrayList<>(Arrays.asList( //Ferreiro
 			    craftaveis.get(3), //Graveto
 			    craftaveis.get(6)  //bronze
 			));
